f0fd4e07134e513bb78ccb60f46d0deb551d937e
ujjwalm9895 <ujjwalm9895@gmail.com>
2025-06-20 00:45:01 +0530

    port

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
             del active_connections[username]
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
